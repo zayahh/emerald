e0200762785181259c516e813d41515f57ea4cd6
zayahh <zayahh@protonmail.com>
2024-05-01 00:07:59 +0200

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+import Footer from "@/components/footer"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
           >
             {children}
           </ThemeProvider>
+          <Footer />
         </body>
       </html>
     </>
---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex items-center justify-center'>🦶</div>
+  )
+}
+
+export default Footer
