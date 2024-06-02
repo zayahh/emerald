030ebdecd5881da4363d322d19114319d25a993f
zayahh <zayahh@protonmail.com>
2024-06-02 13:14:51 +0200

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex items-center justify-center'>🦶</div>
+    <div className='flex items-center justify-center text-secondary'>If you notice any missing runs, contact me on Discord (@zayahh_)</div>
   )
 }
 
